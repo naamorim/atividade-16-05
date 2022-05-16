edd75748ac9864755ab4c1f6de1c05f06be6dab1
naamorim <naamorim2013@hotmail.com>
2022-05-16 20:43:29 -0300

    js

---
+++ JavaScript/script.js
@@ -0,0 +1,26 @@
+//alert("Seja Bem vindo");
+//document.write("Iniciando em JavaScript");
+//prompt("Digite seu nome:");
+//resp = confirm("Será que vai chover?");
+/*var a,b;
+a = 5;
+b = "5 Kg de açucar";
+document.write("O valor da variável A é: "+a);
+document.write("<p>"+"O valor da variável B é: " +b+ "</p>");
+document.write("<p>"+"O valor da variável A dividido por 2 é: " +a/2+"</p>");
+document.write("<p>"+"O Valor da variável B dividido por 2 é: " +b/2+"</p>");*/
+
+/*var a, inteiro, flutuante;
+a = "45.78 Kg de açucar";
+inteiro = parseInt(a);
+flutuante = parseFloat(a);
+document.write("<p>"+ "O valor númerico inteiro da variável é: "+inteiro+"</p>");
+document.write("<p>"+"O valor da variável A dividido por 2 é: " +inteiro/2+"</p>");
+document.write("<p>"+"Ovalor da variável decimal da variável é: "+flutuante+ "</p>");*/
+
+/*var a,b,c;
+a = "olá Pessoal!";
+b = "Seja bem vindos ao JavaScript";
+c = "a + b";
+document.write(c);*/
+
